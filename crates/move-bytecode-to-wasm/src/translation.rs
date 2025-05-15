8adc78f5319fed1d3b62e44f78a8bb9c0303b863
Nicolas Antinori <nicolas_antinori@hotmail.com>
2025-05-15 09:50:07 -0300

    working for u8

--- crates/move-bytecode-to-wasm/src/translation.rs
+++ crates/move-bytecode-to-wasm/src/translation.rs
@@ -1,8 +1,8 @@
 use functions::{
     add_unpack_function_return_values_instructions, prepare_function_return, MappedFunction,
 };
-use intermediate_types::vector::IVector;
 use intermediate_types::IntermediateType;
+use intermediate_types::{simple_integers::IU8, vector::IVector};
 use move_binary_format::file_format::{Bytecode, Constant, SignatureIndex};
 use walrus::{
     ir::{MemArg, StoreKind},
@@ -190,6 +190,29 @@ fn map_bytecode_instruction(
                 memory,
                 allocator,
             );
+        }
+        Bytecode::Add => {
+            let sum_type = if let (Some(t1), Some(t2)) = (types_stack.pop(), types_stack.pop()) {
+                assert_eq!(
+                    t1, t2,
+                    "types stack error: trying two add two different types {t1:?} {t2:?}"
+                );
+                t1
+            } else {
+                panic!("types stack is empty");
+            };
+
+            match sum_type {
+                IntermediateType::IU8 => IU8::add(builder, module_locals),
+                IntermediateType::IU16 => todo!(),
+                IntermediateType::IU32 => todo!(),
+                IntermediateType::IU64 => todo!(),
+                IntermediateType::IU128 => todo!(),
+                IntermediateType::IU256 => todo!(),
+                t => panic!("type stack error: trying to add two {t:?}"),
+            }
+
+            types_stack.push(sum_type);
         }
         _ => panic!("Unsupported instruction: {:?}", instruction),
     }
--- crates/move-bytecode-to-wasm/src/translation/intermediate_types/simple_integers.rs
+++ crates/move-bytecode-to-wasm/src/translation/intermediate_types/simple_integers.rs
@@ -1,4 +1,4 @@
-use walrus::InstrSeqBuilder;
+use walrus::{ir::BinaryOp, InstrSeqBuilder, ModuleLocals};
 
 use crate::wasm_helpers::{load_i32_from_bytes_instructions, load_i64_from_bytes_instructions};
 
@@ -6,12 +6,31 @@ use crate::wasm_helpers::{load_i32_from_bytes_instructions, load_i64_from_bytes_
 pub struct IU8;
 
 impl IU8 {
+    const MAX_VALUE: i32 = u8::MAX as i32;
+
     pub fn load_constant_instructions(
         builder: &mut InstrSeqBuilder,
         bytes: &mut std::vec::IntoIter<u8>,
     ) {
         let bytes = bytes.take(1).collect::<Vec<u8>>();
         load_i32_from_bytes_instructions(builder, &bytes);
+    }
+
+    pub fn add(builder: &mut InstrSeqBuilder, module_locals: &mut ModuleLocals) {
+        let tmp = module_locals.add(walrus::ValType::I32);
+        builder.binop(BinaryOp::I32Add);
+        builder.local_tee(tmp);
+        builder.i32_const(Self::MAX_VALUE);
+        builder.binop(BinaryOp::I32GtU);
+        builder.if_else(
+            Some(walrus::ValType::I32),
+            |then| {
+                then.unreachable();
+            },
+            |else_| {
+                else_.local_get(tmp);
+            },
+        );
     }
 }
 
