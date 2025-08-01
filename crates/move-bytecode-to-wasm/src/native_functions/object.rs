3fcf0225045e1e9ef24d6964dd606bfb9ba985f8
Pedro Llaurado Harvey <llauradopedro@Pedros-MacBook-Pro.local>
2025-08-01 16:55:43 -0300

    move counter to storage

--- crates/move-bytecode-to-wasm/src/native_functions/object.rs
+++ crates/move-bytecode-to-wasm/src/native_functions/object.rs
@@ -1,13 +1,18 @@
 use super::NativeFunction;
 use crate::{
     CompilationContext,
-    hostio::host_functions::{block_number, block_timestamp, native_keccak256},
+    hostio::host_functions::{
+        block_number, block_timestamp, native_keccak256, storage_cache_bytes32,
+        storage_flush_cache, storage_load_bytes32,
+    },
     translation::intermediate_types::address::IAddress,
 };
 use walrus::{
-    ConstExpr, FunctionBuilder, FunctionId, Module, ValType,
-    ir::{BinaryOp, MemArg, StoreKind},
+    FunctionBuilder, FunctionId, Module, ValType,
+    ir::{BinaryOp, LoadKind, MemArg, StoreKind},
 };
+
+use alloy_primitives::keccak256;
 
 pub fn add_native_fresh_id_fn(
     module: &mut Module,
@@ -16,36 +21,101 @@ pub fn add_native_fresh_id_fn(
     let (native_keccak, _) = native_keccak256(module);
     let (block_number, _) = block_number(module);
     let (block_timestamp, _) = block_timestamp(module);
-
-    // NOTE: This global variable is temporal. The counter will be stored in the contract's store.
-    let global_counter = module.globals.add_local(
-        ValType::I32,
-        true,
-        false,
-        ConstExpr::Value(walrus::ir::Value::I32(0)),
-    );
+    let (storage_load_fn, _) = storage_load_bytes32(module);
+    let (storage_cache_fn, _) = storage_cache_bytes32(module);
+    let (storage_flush_cache_fn, _) = storage_flush_cache(module);
 
     let mut function = FunctionBuilder::new(&mut module.types, &[], &[ValType::I32]);
 
     let id_ptr = module.locals.add(ValType::I32);
     let data_to_hash_ptr = module.locals.add(ValType::I32);
+    let counter_key_ptr = module.locals.add(ValType::I32); // Pointer for the storage key
+    let counter_value_ptr = module.locals.add(ValType::I32); // Pointer to receive value read from storage
 
     let mut builder = function
         .name(NativeFunction::NATIVE_FRESH_ID.to_owned())
         .func_body();
 
+    // Counter key
+    builder
+        .i32_const(32)
+        .call(compilation_ctx.allocator)
+        .local_set(counter_key_ptr);
+
+    // Store each byte into linear memory at (counter_key_ptr + offset)
+    let binding = keccak256(b"global_counter_key");
+    let counter_key = binding.as_slice();
+    for (offset, byte) in counter_key.iter().enumerate() {
+        builder.local_get(counter_key_ptr); // base ptr
+        builder.i32_const(*byte as i32); // byte value
+        builder.store(
+            compilation_ctx.memory_id,
+            StoreKind::I32 { atomic: false },
+            MemArg {
+                align: 0,
+                offset: offset as u32,
+            },
+        );
+    }
+
+    // Counter value
+    builder
+        .i32_const(32)
+        .call(compilation_ctx.allocator)
+        .local_set(counter_dest_ptr);
+
+    // ID
     builder
         .i32_const(IAddress::HEAP_SIZE)
         .call(compilation_ctx.allocator)
         .local_set(id_ptr);
 
-    // Use block timestamp + block number + global counter to generate a unique ID
+    // Data to hash: block timestamp (8 bytes) + block number (8 bytes) + storage counter (32 bytes)
     builder
-        .i32_const(16)
+        .i32_const(48)
         .call(compilation_ctx.allocator)
-        .local_tee(data_to_hash_ptr);
+        .local_set(data_to_hash_ptr);
 
+    // Load the counter from storage
     builder
+        .local_get(counter_key_ptr)
+        .local_get(counter_value_ptr)
+        .call(storage_load_fn);
+
+    // Increment the counter and store it in the local variable
+    builder
+        .local_get(counter_value_ptr)
+        .local_get(counter_value_ptr)
+        .load(
+            compilation_ctx.memory_id,
+            LoadKind::I32 { atomic: false },
+            MemArg {
+                align: 0,
+                offset: 0,
+            },
+        )
+        .i32_const(1)
+        .binop(BinaryOp::I32Add)
+        .store(
+            compilation_ctx.memory_id,
+            StoreKind::I32 { atomic: false },
+            MemArg {
+                align: 0,
+                offset: 0,
+            },
+        );
+
+    // Update storage
+    builder
+        .local_get(counter_key_ptr)
+        .local_get(counter_value_ptr)
+        .call(storage_cache_fn)
+        .i32_const(1)
+        .call(storage_flush_cache_fn);
+
+    // Store the block timestamp in the first 8 bytes at #data_to_hash, block number in the next 8 bytes and counter in the last 4 bytes
+    builder
+        .local_get(data_to_hash_ptr)
         .call(block_timestamp)
         .store(
             compilation_ctx.memory_id,
@@ -64,18 +134,17 @@ pub fn add_native_fresh_id_fn(
                 align: 0,
                 offset: 8,
             },
-        );
-
-    // TODO: replace with counter from contract
-    builder
-        .global_get(global_counter)
-        .i32_const(1)
-        .binop(BinaryOp::I32Add)
-        .global_set(global_counter);
-
-    builder
+        )
         .local_get(data_to_hash_ptr)
-        .global_get(global_counter)
+        .local_get(counter_value_ptr)
+        .load(
+            compilation_ctx.memory_id,
+            LoadKind::I32 { atomic: false },
+            MemArg {
+                align: 0,
+                offset: 0,
+            },
+        )
         .store(
             compilation_ctx.memory_id,
             StoreKind::I32 { atomic: false },
@@ -85,9 +154,10 @@ pub fn add_native_fresh_id_fn(
             },
         );
 
+    // Hash the data to generate the ID
     builder
         .local_get(data_to_hash_ptr)
-        .i32_const(20)
+        .i32_const(48)
         .local_get(id_ptr)
         .call(native_keccak);
 
