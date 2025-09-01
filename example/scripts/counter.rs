c15b211b309d0ee304e5cc09423a94003d525bf1
Pedro Llauradó <89028786+batatovich@users.noreply.github.com>
2025-09-01 12:33:20 -0300

    feat: add set value tx with different sender (#131)
    * add set value tx with different sender
    
    * Update example/scripts/counter.rs
    
    Co-authored-by: Nicolás Antinori <92927891+nicoan@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Nicolás Antinori <92927891+nicoan@users.noreply.github.com>

--- example/scripts/counter.rs
+++ example/scripts/counter.rs
@@ -1,5 +1,7 @@
 use alloy::hex;
-use alloy::primitives::FixedBytes;
+use alloy::primitives::{FixedBytes, U256};
+use alloy::providers::Provider;
+use alloy::rpc::types::TransactionRequest;
 use alloy::signers::local::PrivateKeySigner;
 use alloy::{primitives::Address, providers::ProviderBuilder, sol, transports::http::reqwest::Url};
 use dotenv::dotenv;
@@ -27,6 +29,7 @@ async fn main() -> eyre::Result<()> {
         .map_err(|_| eyre!("No {} env var set", "CONTRACT_ADDRESS_COUNTER"))?;
 
     let signer = PrivateKeySigner::from_str(&priv_key)?;
+    let sender = signer.address();
 
     let provider = Arc::new(
         ProviderBuilder::new()
@@ -87,6 +90,41 @@ async fn main() -> eyre::Result<()> {
 
     println!("\nReading value after increment");
     let res = example.read(counter_id).call().await?;
+    println!("counter = {}", res);
+
+    // Add a new sender and try to set the value
+    let priv_key_2 =
+        std::env::var("PRIV_KEY_2").map_err(|_| eyre!("No {} env var set", "PRIV_KEY_2"))?;
+    let signer_2 = PrivateKeySigner::from_str(&priv_key_2)?;
+    let sender_2 = signer_2.address();
+
+    let provider_2 = Arc::new(
+        ProviderBuilder::new()
+            .wallet(signer_2)
+            .with_chain_id(412346)
+            .connect_http(Url::from_str(&rpc_url).unwrap()),
+    );
+    let example_2 = Example::new(address, provider_2.clone());
+
+    println!("\nFunding {sender_2} with some ETH to pay for the gas");
+    let tx = TransactionRequest::default()
+        .from(sender)
+        .to(sender_2)
+        .value(U256::from(5_000_000_000_000_000_000u128)); // 5 eth in wei
+    let pending_tx = provider.send_transaction(tx).await?;
+    pending_tx.get_receipt().await?;
+
+    println!("\nSending set value to 100 tx with the account that is not the owner");
+    let pending_tx = example_2.setValue(counter_id, 100).send().await?;
+    let receipt = pending_tx.get_receipt().await?;
+    for log in receipt.logs() {
+        let raw = log.data().data.0.clone();
+        println!("set value logs 0: 0x{}", hex::encode(raw));
+    }
+
+    // Value did not change as the sender is not the owner
+    println!("\nReading value after set value");
+    let res = example_2.read(counter_id).call().await?;
     println!("counter = {}", res);
 
     Ok(())
