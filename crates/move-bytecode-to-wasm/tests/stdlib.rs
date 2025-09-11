5c4782ced78153c7113f02380af05e9cb2ea8dc9
Pedro Llauradó <pllaurado1997@gmail.com>
2025-09-11 15:29:32 -0300

    fix stdlib test case

--- crates/move-bytecode-to-wasm/tests/stdlib.rs
+++ crates/move-bytecode-to-wasm/tests/stdlib.rs
@@ -6,7 +6,6 @@ mod common;
 /// Bytecode.
 /// We can't translate it all to WASM yet so it should panic!
 #[test]
-#[should_panic]
 fn test_use_stdlib() {
     const MODULE_NAME: &str = "use_stdlib";
     const SOURCE_PATH: &str = "tests/stdlib/use_stdlib.move";
