5d7ce35706c3f5782eb85c151a7d6d9bf36913fb
Nicolas Antinori <nicolas_antinori@hotmail.com>
2025-09-12 14:09:32 -0300

    WIP

---
+++ crates/move-cli/src/base/abi_generate.rs
@@ -0,0 +1,21 @@
+use super::reroot_path;
+use clap::*;
+use move_bytecode_to_wasm::translate_package_cli;
+use move_evm_abi_generator::generate_abi;
+use move_package::BuildConfig;
+use std::path::Path;
+
+/// Build the package at `path`. If no path is provided defaults to current directory.
+#[derive(Parser)]
+#[clap(name = "abi-generate")]
+pub struct AbiGenerate;
+
+impl AbiGenerate {
+    pub fn execute(self, path: Option<&Path>, config: BuildConfig) -> anyhow::Result<()> {
+        generate_abi(path);
+
+        println!("{path:?}");
+
+        Ok(())
+    }
+}
