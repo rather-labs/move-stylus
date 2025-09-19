0658178f4fcd5a37e9ebc0a8c79ac15bcdf31781
Nicolas Antinori <nicolas_antinori@hotmail.com>
2025-09-19 09:43:10 -0300

    WIP: unpacking named ids

---
+++ crates/move-bytecode-to-wasm/src/vm_handled_types/named_id.rs
@@ -0,0 +1,47 @@
+use super::VmHandledType;
+use crate::{
+    CompilationContext,
+    compilation_context::{ModuleId, reserved_modules::STYLUS_FRAMEWORK_ADDRESS},
+};
+use walrus::{InstrSeqBuilder, Module};
+
+pub struct NamedId;
+
+impl VmHandledType for NamedId {
+    const IDENTIFIER: &str = "NamedId";
+
+    fn inject(
+        _block: &mut InstrSeqBuilder,
+        _module: &mut Module,
+        _compilation_ctx: &CompilationContext,
+    ) {
+        // UID is not injected, is created with a native function
+    }
+
+    fn is_vm_type(module_id: &ModuleId, index: u16, compilation_ctx: &CompilationContext) -> bool {
+        let identifier = &compilation_ctx
+            .get_struct_by_index(module_id, index)
+            .unwrap()
+            .identifier;
+
+        if identifier == Self::IDENTIFIER {
+            if module_id.address != STYLUS_FRAMEWORK_ADDRESS || module_id.module_name != "object" {
+                panic!("invalid NamedId found, only the one from the stylus framework is valid");
+            }
+            return true;
+        }
+        false
+    }
+}
+
+/*
+impl NamedId {
+    const DELETE_FN_IDENTIFIER: &str = "delete";
+
+    pub fn is_delete_function(module_id: &ModuleId, identifier: &str) -> bool {
+        identifier == Self::DELETE_FN_IDENTIFIER
+            && module_id.address == STYLUS_FRAMEWORK_ADDRESS
+            && module_id.module_name == "object"
+    }
+}
+*/
