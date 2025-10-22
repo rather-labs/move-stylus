54a2237a40d49dffff5e94cb210ca6380e08a4da
Pedro Llauradó <pllaurado1997@gmail.com>
2025-10-22 14:55:11 -0300

    add fields mutations to tests

--- crates/move-bytecode-to-wasm/tests/enums.rs
+++ crates/move-bytecode-to-wasm/tests/enums.rs
@@ -612,22 +612,31 @@ mod structs_with_enums {
         struct Star {
             string name;
             StarType class;
-            uint64 distance;
             Core core;
             uint32 size;
         }
-        function createStar(string name, StarType class, uint64 distance, Core core, uint32 size) external returns (Star);
-        function testStar(string name, StarType class, uint64 distance, Core core, uint32 size, StarType new_class, Core new_core) external returns (Star);
+        function createStar(string name, StarType class, Core core, uint32 size) external returns (Star);
+        function evolveStar(Star star) external returns (Star);
+        function getCoreProperties(Star star) external returns (uint8, uint8);
     }
 
     #[rstest]
-    #[case(createStarCall::new((String::from("Sun"), StarType::YellowDwarf, 42, Core::Hydrogen, 55)), Star { name: String::from("Sun"), class: StarType::YellowDwarf, distance: 42, core: Core::Hydrogen, size: 55 })]
-    #[case(createStarCall::new((String::from("Proxima Centauri"), StarType::RedDwarf, 4, Core::Helium, 1)), Star { name: String::from("Proxima Centauri"), class: StarType::RedDwarf, distance: 4, core: Core::Helium, size: 1 })]
-    #[case(createStarCall::new((String::from("Betelgeuse"), StarType::RedGiant, 642, Core::Carbon, 764)), Star { name: String::from("Betelgeuse"), class: StarType::RedGiant, distance: 642, core: Core::Carbon, size: 764 })]
-    #[case(createStarCall::new((String::from("Vega"), StarType::BlueGiant, 25, Core::Nitrogen, 2)), Star { name: String::from("Vega"), class: StarType::BlueGiant, distance: 25, core: Core::Nitrogen, size: 2 })]
-    #[case(createStarCall::new((String::from("Polaris"), StarType::YellowDwarf, 433, Core::Oxygen, 37)), Star { name: String::from("Polaris"), class: StarType::YellowDwarf, distance: 433, core: Core::Oxygen, size: 37 })]
-    #[case(testStarCall::new((String::from("Sun"), StarType::YellowDwarf, 42, Core::Hydrogen, 55, StarType::RedDwarf, Core::Helium)), Star { name: String::from("Sun"), class: StarType::RedDwarf, distance: 42, core: Core::Helium, size: 55 })]
-    #[case(testStarCall::new((String::from("Proxima Centauri"), StarType::RedDwarf, 4, Core::Helium, 1, StarType::YellowDwarf, Core::Hydrogen)), Star { name: String::from("Proxima Centauri"), class: StarType::YellowDwarf, distance: 4, core: Core::Hydrogen, size: 1 })]
+    #[case(createStarCall::new((String::from("Sun"), StarType::YellowDwarf, Core::Hydrogen, 55)), Star { name: String::from("Sun"), class: StarType::YellowDwarf, core: Core::Hydrogen, size: 55 })]
+    #[case(createStarCall::new((String::from("Proxima Centauri"), StarType::RedDwarf, Core::Helium, 1)), Star { name: String::from("Proxima Centauri"), class: StarType::RedDwarf, core: Core::Helium, size: 1 })]
+    #[case(createStarCall::new((String::from("Betelgeuse"), StarType::RedGiant, Core::Carbon, 764)), Star { name: String::from("Betelgeuse"), class: StarType::RedGiant, core: Core::Carbon, size: 764 })]
+    #[case(createStarCall::new((String::from("Vega"), StarType::BlueGiant, Core::Nitrogen, 2)), Star { name: String::from("Vega"), class: StarType::BlueGiant, core: Core::Nitrogen, size: 2 })]
+    #[case(createStarCall::new((String::from("Polaris"), StarType::YellowDwarf, Core::Oxygen, 37)), Star { name: String::from("Polaris"), class: StarType::YellowDwarf, core: Core::Oxygen, size: 37 })]
+    #[case(evolveStarCall::new((Star { name: String::from("Sun"), class: StarType::YellowDwarf, core: Core::Hydrogen, size: 55 },)), Star { name: String::from("Sun"), class: StarType::RedGiant, core: Core::Helium, size: 5500 })]
+    #[case(evolveStarCall::new((Star { name: String::from("Proxima Centauri"), class: StarType::RedDwarf, core: Core::Helium, size: 1 },)), Star { name: String::from("Proxima Centauri"), class: StarType::RedGiant, core: Core::Carbon, size: 2 })]
+    #[case(evolveStarCall::new((Star { name: String::from("Betelgeuse"), class: StarType::RedGiant, core: Core::Carbon, size: 764 },)), Star { name: String::from("Betelgeuse"), class: StarType::BlueGiant, core: Core::Nitrogen, size: 3820 })]
+    #[case(evolveStarCall::new((Star { name: String::from("Vega"), class: StarType::BlueGiant, core: Core::Nitrogen, size: 2 },)), Star { name: String::from("Vega"), class: StarType::BlueGiant, core: Core::Oxygen, size: 6 })]
+    #[should_panic]
+    #[case(evolveStarCall::new((Star { name: String::from("Polaris"), class: StarType::YellowDwarf, core: Core::Oxygen, size: 37 },)), Star { name: String::from("Polaris"), class: StarType::BlueGiant, core: Core::Oxygen, size: 111 })]
+    #[case(getCorePropertiesCall::new((Star { name: String::from("Sun"), class: StarType::YellowDwarf, core: Core::Hydrogen, size: 55 },)), (1, 1))]
+    #[case(getCorePropertiesCall::new((Star { name: String::from("Proxima Centauri"), class: StarType::RedDwarf, core: Core::Helium, size: 1 },)), (2, 18))]
+    #[case(getCorePropertiesCall::new((Star { name: String::from("Betelgeuse"), class: StarType::RedGiant, core: Core::Carbon, size: 764 },)), (6, 14))]
+    #[case(getCorePropertiesCall::new((Star { name: String::from("Vega"), class: StarType::BlueGiant, core: Core::Nitrogen, size: 2 },)), (7, 15))]
+    #[case(getCorePropertiesCall::new((Star { name: String::from("Polaris"), class: StarType::YellowDwarf, core: Core::Oxygen, size: 37 },)), (8, 16))]
     fn test_star<T: SolCall, V: SolValue>(
         #[by_ref] runtime: &RuntimeSandbox,
         #[case] call_data: T,
--- crates/move-bytecode-to-wasm/tests/enums/structs_with_enums.move
+++ crates/move-bytecode-to-wasm/tests/enums/structs_with_enums.move
@@ -2,7 +2,9 @@ module 0x00::structs_with_enums;
 use std::ascii::String;
 use std::ascii as ascii;
 
-public enum Core has drop {
+const E_SUPERNOVA: u64 = 1;
+
+public enum Core has drop, copy {
     Hydrogen,
     Helium,
     Carbon,
@@ -10,36 +12,64 @@ public enum Core has drop {
     Oxygen,
 }
 
-public enum StarType has drop {
+public enum StarType has drop, copy {
     RedDwarf,
     YellowDwarf,
     RedGiant,
     BlueGiant,
 }
 
-public struct Star has drop {
+public struct Star has drop, copy {
     name: String,
     class: StarType,
-    distance: u64,
     core: Core,
     size: u32,
 }
 
-entry fun create_star(name: String, class: StarType, distance: u64, core: Core, size: u32): Star {
-    Star { name, class, distance, core, size }
+/// Creates a new star with the given properties.
+entry fun create_star(name: String, class: StarType, core: Core, size: u32): Star {
+    Star { name, class, core, size }
 }
 
-fun change_star_class(star: &mut Star, new_class: StarType) {
-    star.class = new_class;
+/// Evolves the star's core to the next element in the periodic table.
+/// This also changes the star's class and size according to stellar evolution.
+entry fun evolve_star(star: &mut Star): Star {
+    match (&star.core) {
+        Core::Hydrogen => {
+            star.core = Core::Helium;
+            star.class = StarType::RedGiant;  // Hydrogen depletion leads to red giant phase
+            star.size = star.size * 100;     // Stars expand dramatically as red giants
+        },
+        Core::Helium => {
+            star.core = Core::Carbon;
+            star.class = StarType::RedGiant;  // Still in red giant phase
+            star.size = star.size * 2;        // Further expansion
+        },
+        Core::Carbon => {
+            star.core = Core::Nitrogen;
+            star.class = StarType::BlueGiant; // More massive stars become blue giants
+            star.size = star.size * 5;        // Blue giants are very large
+        },
+        Core::Nitrogen => {
+            star.core = Core::Oxygen;
+            star.class = StarType::BlueGiant; // Still blue giant
+            star.size = star.size * 3;        // Continued expansion
+        },
+        Core::Oxygen => {
+            // If already at last, star collapses (supernova)
+            abort(E_SUPERNOVA)
+        },
+    };
+    *star
 }
 
-fun change_star_core(star: &mut Star, new_core: Core) {
-    star.core = new_core;
-}
-
-entry fun test_star(name: String, class: StarType, distance: u64, core: Core, size: u32, new_class: StarType, new_core: Core): Star {
-    let mut star = create_star(name, class, distance, core, size);
-    change_star_class(&mut star, new_class);
-    change_star_core(&mut star, new_core);
-    star
+/// Returns (atomic_number, group_number) for the star's core element.
+entry fun get_core_properties(star: &Star): (u8, u8) {
+    match (&star.core) {
+        Core::Hydrogen => (1, 1),
+        Core::Helium => (2, 18),
+        Core::Carbon => (6, 14),
+        Core::Nitrogen => (7, 15),
+        Core::Oxygen => (8, 16),
+    }
 }
