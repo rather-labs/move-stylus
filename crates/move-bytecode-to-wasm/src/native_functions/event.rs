ae2f43f9ffc2fa5110e36774863cd31beca70a27
Nicolas Antinori <nicolas_antinori@hotmail.com>
2025-11-27 14:50:06 -0300

    fix events

--- crates/move-bytecode-to-wasm/src/native_functions/event.rs
+++ crates/move-bytecode-to-wasm/src/native_functions/event.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+use move_binary_format::file_format::FieldHandleIndex;
 use walrus::{
     FunctionBuilder, FunctionId, Module, ValType,
     ir::{BinaryOp, LoadKind, MemArg},
@@ -8,7 +11,10 @@ use crate::{
     abi_types::{event_encoding::move_signature_to_event_signature_hash, packing::Packable},
     compilation_context::ModuleId,
     hostio::host_functions::{emit_log, native_keccak256},
-    translation::intermediate_types::{IntermediateType, structs::IStructType},
+    translation::intermediate_types::{
+        IntermediateType,
+        structs::{IStruct, IStructType},
+    },
 };
 
 use super::{NativeFunction, error::NativeFunctionError};
@@ -21,7 +27,7 @@ use super::{NativeFunction, error::NativeFunctionError};
 /// - If a dynamic structure is part of a topic, its Keccak-256 hash is computed over the encoded
 ///   memory region and placed in the corresponding topic.
 /// - Otherwise, if it is part of the event data, the encoded memory is copied into the data section
-///   of the eve
+///   of the event
 pub fn add_emit_log_fn(
     module: &mut Module,
     compilation_ctx: &CompilationContext,
@@ -249,14 +255,12 @@ pub fn add_emit_log_fn(
     }
 
     // ABI pack the struct before emitting the event
-    for ((field_index, field), abi_encoded_data) in struct_
-        .fields
+    // First process the indexed ones
+    for ((field_index, field), abi_encoded_data) in struct_.fields[0..indexes as usize]
         .iter()
         .enumerate()
         .zip(event_fields_encoded_data)
     {
-        let is_indexed = field_index < indexes as usize;
-
         // Get the pointer to the field
         builder.local_get(struct_ptr).load(
             compilation_ctx.memory_id,
@@ -339,43 +343,20 @@ pub fn add_emit_log_fn(
 
                 // If the vector is indexed, we need to calculate the keccak256 of its values and
                 // store them in the topic
-                if is_indexed {
-                    builder.i32_const(32).call(compilation_ctx.allocator).drop();
+                builder.i32_const(32).call(compilation_ctx.allocator).drop();
 
-                    builder
-                        .local_get(encode_start)
-                        .local_get(abi_encoded_data_length)
-                        .local_get(writer_pointer)
-                        .call(native_keccak);
+                builder
+                    .local_get(encode_start)
+                    .local_get(abi_encoded_data_length)
+                    .local_get(writer_pointer)
+                    .call(native_keccak);
 
-                    builder
-                        .i32_const(32)
-                        .local_get(writer_pointer)
-                        .binop(BinaryOp::I32Add)
-                        .local_tee(writer_pointer)
-                        .local_set(calldata_reference_pointer);
-                } else {
-                    builder
-                        .local_get(abi_encoded_data_length)
-                        .call(compilation_ctx.allocator)
-                        .drop();
-
-                    // Copy the abi encoded data to its place in the event data
-                    builder
-                        .local_get(writer_pointer)
-                        .local_get(encode_start)
-                        .local_get(abi_encoded_data_length)
-                        .memory_copy(compilation_ctx.memory_id, compilation_ctx.memory_id);
-
-                    // Update the calldata reference to the start of the new field (vector encoding
-                    // moves the writer pointer to the end of the encoding)
-                    builder
-                        .local_get(writer_pointer)
-                        .local_get(abi_encoded_data_length)
-                        .binop(BinaryOp::I32Add)
-                        .local_tee(writer_pointer)
-                        .local_set(calldata_reference_pointer);
-                }
+                builder
+                    .i32_const(32)
+                    .local_get(writer_pointer)
+                    .binop(BinaryOp::I32Add)
+                    .local_tee(writer_pointer)
+                    .local_set(calldata_reference_pointer);
             }
             IntermediateType::IEnum { .. } | IntermediateType::IGenericEnumInstance { .. } => {
                 todo!()
@@ -386,6 +367,56 @@ pub fn add_emit_log_fn(
                 ));
             }
         }
+    }
+
+    // For the data left, we need to generate a "fake" IStruct with the fields that are not
+    // included in the topics.
+    let fields: Vec<(Option<FieldHandleIndex>, IntermediateType)> = struct_.fields
+        [indexes as usize..]
+        .iter()
+        .map(|t| (None, t.clone()))
+        .collect();
+
+    let data_struct = IStruct::new(
+        move_binary_format::file_format::StructDefinitionIndex(0),
+        "DataStruct".to_owned(),
+        fields,
+        HashMap::new(),
+        false,
+        IStructType::Common,
+    );
+
+    let abi_encoded_data_writer_pointer = module.locals.add(ValType::I32);
+    let abi_encoded_data_calldata_reference_pointer = module.locals.add(ValType::I32);
+
+    let data_struct_ptr = module.locals.add(ValType::I32);
+
+    builder
+        .local_get(struct_ptr)
+        .i32_const(4 * indexes as i32)
+        .binop(BinaryOp::I32Add)
+        .local_set(data_struct_ptr);
+
+    if data_struct.solidity_abi_encode_is_dynamic(compilation_ctx)? {
+        data_struct.add_pack_instructions(
+            &mut builder,
+            module,
+            data_struct_ptr,
+            abi_encoded_data_writer_pointer,
+            abi_encoded_data_calldata_reference_pointer,
+            compilation_ctx,
+            Some(abi_encoded_data_calldata_reference_pointer),
+        )?;
+    } else {
+        data_struct.add_pack_instructions(
+            &mut builder,
+            module,
+            data_struct_ptr,
+            abi_encoded_data_writer_pointer,
+            abi_encoded_data_calldata_reference_pointer,
+            compilation_ctx,
+            None,
+        )?;
     }
 
     // Beginning of the packed data
