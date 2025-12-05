743c9ee41fe3f143738d6ce50bc858ecb9d0659d
Nicolas Antinori <nicolas_antinori@hotmail.com>
2025-12-05 16:41:05 -0300

    WIP: reformatted

--- crates/move-test-runner/src/lib.rs
+++ crates/move-test-runner/src/lib.rs
@@ -1,15 +1,10 @@
 mod constants;
 mod wasm_runner;
 
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use move_bytecode_to_wasm::compilation_context::{ModuleData, ModuleId};
 use wasm_runner::RuntimeSandbox;
-
-pub struct Summary {
-    module: PathBuf,
-    failed_tests: Vec<String>,
-}
 
 const RESET: &str = "\x1b[0m";
 const GREEN: &str = "\x1b[32m";
@@ -21,9 +16,9 @@ pub fn run_tests(
     module_data: &ModuleData,
     module_path: &Path,
     compiled_modules_path: &Path,
-) -> Summary {
+) {
     println!(
-        "\n[ RUNNING {CYAN}{module_id}{RESET} TESTS ({})]",
+        "\nRunning {CYAN}{module_id}{RESET} tests ({})]\n",
         module_path.display()
     );
 
@@ -32,34 +27,32 @@ pub fn run_tests(
         .join(&module_id.module_name);
     compiled_wasm.set_extension("wasm");
 
-    println!("------------------------------------------------------------");
     let mut failures = Vec::new();
     for test in &module_data.special_attributes.test_functions {
+        print!("  {module_id}::{test} ... ");
         let runtime = RuntimeSandbox::new(&compiled_wasm);
         let result = runtime.call_test_function(test);
-
         if result.is_ok() {
-            println!("+ {module_id}::{test:50} {GREEN}PASSED{RESET}");
+            println!("{GREEN}PASSED{RESET}");
         } else {
-            println!("- {module_id}::{test:50} {RED}FAILED{RESET}");
+            println!("{RED}FAILED{RESET}");
             failures.push(test.to_owned());
         }
     }
-    println!("------------------------------------------------------------");
 
     let total = module_data.special_attributes.test_functions.len();
-    println!("\n[ SUMMARY ]",);
-    println!("------------------------------------------------------------");
-    println!(
-        "Total Tests : {}",
+
+    print!(
+        "\nTotal Tests : {}, ",
         module_data.special_attributes.test_functions.len()
     );
-    println!("{GREEN}Passed{RESET}      : {}", total - failures.len(),);
-    println!("{RED}Failed{RESET}      : {}", failures.len());
-    println!("------------------------------------------------------------");
+    print!("{GREEN}Passed{RESET}: {}, ", total - failures.len(),);
+    println!("{RED}Failed{RESET}: {}.", failures.len());
 
-    Summary {
-        module: module_path.to_path_buf(),
-        failed_tests: failures,
+    if !failures.is_empty() {
+        println!("Failed tests:");
+        for failed_test in failures {
+            println!("  {module_id}::{failed_test}");
+        }
     }
 }
