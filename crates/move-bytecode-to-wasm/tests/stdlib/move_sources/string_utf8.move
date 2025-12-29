9ff91a65726f91e377973520ec72f99a7223726c
Nicolas Antinori <nicolas_antinori@hotmail.com>
2025-12-29 12:22:01 -0300

    WIP: tests

--- crates/move-bytecode-to-wasm/tests/stdlib/move_sources/string_utf8.move
+++ crates/move-bytecode-to-wasm/tests/stdlib/move_sources/string_utf8.move
@@ -5,3 +5,56 @@ use std::string::{String, utf8};
 entry fun pack_utf8(): String {
      utf8(b"hello world")
 }
+
+
+entry fun pack_utf8_2(): (String, String) {
+    (
+        utf8(b"hello world"),
+        utf8(b"test string"),
+    )
+}
+
+entry fun pack_utf8_3(): (String, u16, String) {
+    (
+        utf8(b"hello world"),
+        42,
+        utf8(b"test string"),
+    )
+}
+
+entry fun pack_utf8_4(): (String, vector<u16>, String) {
+    (
+        utf8(b"hello world"),
+        vector[3, 1, 4, 1, 5],
+        utf8(b"test string"),
+    )
+}
+
+entry fun pack_unpack_utf8(value: String): String {
+    value
+}
+
+entry fun pack_unpack_utf8_2(value: String, value_2: String): (String, String) {
+    (value, value_2)
+}
+
+entry fun unpack_utf8(value: String): bool {
+    value.as_bytes() == b"dlrow olleh"
+}
+
+entry fun unpack_utf8_2(value: String, value_2: String): bool {
+    value.as_bytes() == b"hello world"
+        && value_2.as_bytes() == b"test string"
+}
+
+entry fun unpack_utf8_3(value: String, n: u16, value_2: String): bool {
+    value.as_bytes() == b"hello world"
+        && n == 42
+        && value_2.as_bytes() == b"test string"
+}
+
+entry fun unpack_utf8_4(value: String, n: vector<u16>, value_2: String): bool {
+    value.as_bytes() == b"hello world"
+        && n == vector[3, 1, 4, 1, 5]
+        && value_2.as_bytes() == b"test string"
+}
--- crates/move-bytecode-to-wasm/tests/stdlib/string_utf8.rs
+++ crates/move-bytecode-to-wasm/tests/stdlib/string_utf8.rs
@@ -9,8 +9,18 @@ declare_fixture!("string_utf8", "tests/stdlib/move_sources/string_utf8.move");
 sol!(
     #[allow(missing_docs)]
     function packUtf8() external returns (string);
+    function packUtf82() external returns (string, string);
+    function packUtf83() external returns (string, uint16, string);
+    function packUtf84() external returns (string, uint16[], string);
+    function unpackUtf8(string value) external returns (bool);
+    function unpackUtf82(string value, string value2) external returns (bool);
+    function unpackUtf83(string value, uint16 n, string value2) external returns (bool);
+    function unpackUtf84(string value, uint16[] n, string value2) external returns (bool);
+    function packUnpackUtf8(string value) external returns (string);
+    function packUnpackUtf82(string value, string value2) external returns (string, string);
 );
 
+/*
 #[rstest]
 #[case(packUtf8Call::new(()), "hello world")]
 fn test_utf8<T: SolCall, V: SolValue>(
@@ -22,6 +32,63 @@ fn test_utf8<T: SolCall, V: SolValue>(
         runtime,
         call_data.abi_encode(),
         expected_result.abi_encode(),
+    )
+    .unwrap();
+}
+*/
+#[rstest]
+#[case(packUtf8Call::new(()), "hello world")]
+#[case(unpackUtf8Call::new(("dlrow olleh".to_owned(),)), true)]
+#[case(unpackUtf82Call::new((
+        "hello world".to_owned(),
+        "test string".to_owned(),
+    )), true)]
+#[case(unpackUtf83Call::new((
+        "hello world".to_owned(),
+        42,
+        "test string".to_owned(),
+    )), true)]
+#[case(unpackUtf84Call::new((
+        "hello world".to_owned(),
+        vec![3,1,4,1,5],
+        "test string".to_owned(),
+    )), true)]
+#[case(packUnpackUtf8Call::new(("test string".to_owned(),)), "test string")]
+fn test_utf8<T: SolCall, V: SolValue>(
+    #[by_ref] runtime: &RuntimeSandbox,
+    #[case] call_data: T,
+    #[case] expected_result: V,
+) {
+    run_test(
+        runtime,
+        call_data.abi_encode(),
+        expected_result.abi_encode(),
+    )
+    .unwrap();
+}
+
+#[rstest]
+#[case(packUtf82Call::new(()), ("hello world", "test string"))]
+#[case(packUtf83Call::new(()), ("hello world", 42, "test string"))]
+#[case(packUtf84Call::new(()), ("hello world", vec![3,1,4,1,5], "test string"))]
+#[case(packUnpackUtf82Call::new((
+        "test string".to_owned(),
+        "hello world".to_owned()
+    )), (
+        "test string",
+        "hello world",
+    ))]
+fn test_utf8_multiple<T: SolCall, V: SolValue>(
+    #[by_ref] runtime: &RuntimeSandbox,
+    #[case] call_data: T,
+    #[case] expected_result: V,
+) where
+    for<'a> <V::SolType as SolType>::Token<'a>: TokenSeq<'a>,
+{
+    run_test(
+        runtime,
+        call_data.abi_encode(),
+        expected_result.abi_encode_sequence(),
     )
     .unwrap();
 }
