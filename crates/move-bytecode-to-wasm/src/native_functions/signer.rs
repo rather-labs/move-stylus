a6172aefbbbfaf9f227fd5229fecedc5b485f669
Pedro Llauradó <pllaurado1997@gmail.com>
2026-02-18 16:34:44 -0300

    copy signer address in fresh memory

--- crates/move-bytecode-to-wasm/src/native_functions/signer.rs
+++ crates/move-bytecode-to-wasm/src/native_functions/signer.rs
@@ -6,14 +6,17 @@
 //! This function returns the address of the signer.
 use super::NativeFunction;
 use crate::{
-    CompilationContext, compilation_context::ModuleId, native_functions::error::NativeFunctionError,
+    CompilationContext, compilation_context::ModuleId,
+    native_functions::error::NativeFunctionError, translation::intermediate_types::signer::ISigner,
 };
 use walrus::{FunctionBuilder, FunctionId, Module, ValType};
 
-// This function simply echoes the signer pointer.
+/// Returns the signer's address.
+///
+/// It allocates fresh memory and copy the 32-byte address data into it.
 pub fn add_signer_address_of_fn(
     module: &mut Module,
-    _compilation_ctx: &CompilationContext,
+    compilation_ctx: &CompilationContext,
     module_id: &ModuleId,
 ) -> Result<FunctionId, NativeFunctionError> {
     let mut function = FunctionBuilder::new(&mut module.types, &[ValType::I32], &[ValType::I32]);
@@ -25,8 +28,19 @@ pub fn add_signer_address_of_fn(
         .func_body();
 
     let signer_ptr = module.locals.add(ValType::I32);
+    let new_ptr = module.locals.add(ValType::I32);
 
-    builder.local_get(signer_ptr);
+    // Allocate 32 bytes and copy the signer's address into it.
+    builder
+        .i32_const(ISigner::HEAP_SIZE)
+        .call(compilation_ctx.allocator)
+        .local_tee(new_ptr)
+        .local_get(signer_ptr)
+        .i32_const(ISigner::HEAP_SIZE)
+        .memory_copy(compilation_ctx.memory_id, compilation_ctx.memory_id);
+
+    // Return the new, independent pointer.
+    builder.local_get(new_ptr);
 
     Ok(function.finish(vec![signer_ptr], &mut module.funcs))
 }
