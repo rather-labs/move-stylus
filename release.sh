d01829654c74db0fb75f0715548647661a0b598e
Nicolás Antinori <92927891+nicoan@users.noreply.github.com>
2026-03-03 19:28:09 -0300

    add: release actions (#311)

--- release.sh
+++ release.sh
@@ -4,45 +4,7 @@ PACKAGE_VERSION=$(cat ./crates/move-cli/Cargo.toml | grep version | head -n 1 | 
 RED="\033[0;31m"
 NC="\033[0m"
 
-echo "Releasing move-cli version $PACKAGE_VERSION"
-
-# Check that the artifacts and SHA256 checksums are in place
-
-MACOS_ARTIFACT="./dist/move-stylus-aarch64-macos-${PACKAGE_VERSION}.tar.gz"
-if [ ! -f "$MACOS_ARTIFACT" ]; then
-    echo -e "${RED}Error${NC}: Artifact for macOS is missing: $MACOS_ARTIFACT"
-    exit 1
-fi
-
-MACOS_CHECKSUM="./dist/move-stylus-aarch64-macos-${PACKAGE_VERSION}.tar.gz.sha256"
-if [ ! -f "$MACOS_CHECKSUM" ]; then
-    echo -e "${RED}Error${NC}: Checksum for macOS' artifact is missing: $MACOS_CHECKSUM"
-    exit 1
-fi
-
-LINUX_X86_ARTIFACT="./dist/move-stylus-x86_64-linux-${PACKAGE_VERSION}.tar.gz"
-if [ ! -f "$LINUX_X86_ARTIFACT" ]; then
-    echo -e "${RED}Error${NC}: Artifact for Linux x86_64 is missing: $LINUX_X86_ARTIFACT"
-    exit 1
-fi
-
-LINUX_X86_CHECKSUM="./dist/move-stylus-x86_64-linux-${PACKAGE_VERSION}.tar.gz.sha256"
-if [ ! -f "$LINUX_X86_CHECKSUM" ]; then
-    echo -e "${RED}Error${NC}: Checksum for Linux x86_64 artifact is missing: $LINUX_X86_CHECKSUM"
-    exit 1
-fi
-
-LINUX_ARM_ARTIFACT="./dist/move-stylus-aarch64-linux-${PACKAGE_VERSION}.tar.gz"
-if [ ! -f "$LINUX_ARM_ARTIFACT" ]; then
-    echo -e "${RED}Error${NC}: Artifact for Linux aarch64 is missing: $LINUX_ARM_ARTIFACT"
-    exit 1
-fi
-
-LINUX_ARM_CHECKSUM="./dist/move-stylus-aarch64-linux-${PACKAGE_VERSION}.tar.gz.sha256"
-if [ ! -f "$LINUX_ARM_CHECKSUM" ]; then
-    echo -e "${RED}Error${NC}: Checksum for Linux aarch64 artifact is missing: $LINUX_ARM_CHECKSUM"
-    exit 1
-fi
+echo "Releasing move-stylus version $PACKAGE_VERSION"
 
 # Creating a git tag for the release
 GIT_TAG="v$PACKAGE_VERSION"
@@ -56,4 +18,3 @@ git tag -a "$GIT_TAG" -m "Release version $PACKAGE_VERSION"
 git push origin "$GIT_TAG"
 
 echo "Release $PACKAGE_VERSION created successfully with git tag $GIT_TAG."
-echo "Please remember to create a GitHub release and upload the artifacts and their checksums."
